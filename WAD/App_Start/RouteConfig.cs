2026f6789b72a58c1801c737763e03fb59faa1bb
kimthanun <thanunkim168@gmail.com>
2018-12-18 12:04:43 +0700

    Web asp.net

--- WAD/App_Start/RouteConfig.cs
+++ WAD/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WAD
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "M3", action = "Default", id = UrlParameter.Optional }
             );
         }
     }
---
+++ WAD/Controllers/M3Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WAD.Controllers
+{
+    public class M3Controller : Controller
+    {
+        // GET: M3
+        public string Default()
+        {
+            return "It's a <strong>Default|Index</strong> action method...";
+        }
+        public string Welcome(int stuID=2,string stuName="Linda")
+        {
+
+            return "Hi, " + stuName + "<h3>[" + stuID + "]</h3>";
+        }
+    }
+}
